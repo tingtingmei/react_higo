0d9293ff9b7644538c72a6500d19591d6c5dcfe1
MissOcean <704772438@qq.com>
2017-09-29 16:55:13 +0800

    add /gitignore init project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<div>App</div>, document.getElementById('root'));
+registerServiceWorker();
