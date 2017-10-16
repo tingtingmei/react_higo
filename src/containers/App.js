92bc2c0502e37b75d24199eaac5b15c19292e42b
MissOcean <704772438@qq.com>
2017-10-16 16:08:42 +0800

    组织项目目录

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+/* App */
+import React, {Component} from 'react';
+import './App.css'
+export default class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <span>App</span>
+            </div>
+        )
+    }
+}
