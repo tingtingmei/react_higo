1db4bb7ae08c03d7ca122bdc7c3f32a618093ddd
MissOcean <704772438@qq.com>
2017-10-16 17:28:47 +0800

    配置redux和router,修改babel配置

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,24 @@
 /* App */
 import React, {Component} from 'react';
-import './App.less'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
+import Home from "./Home/index";
+import Cart from "./Cart/index";
+import Topic from "./Topic/index";
+import Category from "./Category/index";
+import User from "./User/index";
+//import {ConnectedRouter} from 'react-router-redux'
 export default class App extends Component {
     render() {
         return (
-            <div className="container">
-                <span>App</span>
-            </div>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home}></Route>
+                    <Route path="/topic" component={Topic}></Route>
+                    <Route path="/cart" component={Cart}></Route>
+                    <Route path="/user" component={User}></Route>
+                    <Route path="/category" component={Category}></Route>
+                </Switch>
+            </Router>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from "./containers/App";
+import store from './store';
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+    <App/>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
